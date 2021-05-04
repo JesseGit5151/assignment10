03ab7cfd2d6dcea860245a3eceb7255b8236f8e2
JesseGit5151 <jrodriguez5151yahoo.com>
2021-05-04 11:52:06 -0700

    update

---
+++ beginning/js/strings.js
@@ -0,0 +1,35 @@
+//STEP 1
+// let name = prompt('Enter your name.')
+// alert(`You have ${name.length} characters in your name.`)
+//STEP 2
+// let userName = prompt('Enter your name.')
+// let userNum = prompt('Enter a number within the length of your name.')
+// alert(`${userName.charAt(userNum)} `)
+//STEP 3
+// let firstName = prompt('Enter your first name.')
+// let lastName = prompt('Enter your last name.')
+// alert(`Your name is: ${firstName} ${lastName}`)
+//STEP 4
+// let string = "The quick brown fox jumps over the lazy dog"
+// alert(`${string.indexOf('fox')}`)
+//STEP 5
+// let string = "The quick brown fox jumps over the lazy fox"
+// alert(`${string.lastIndexOf('fox')}`)
+//STEP 6
+// let string = "The quick brown fox jumped over the lazy dog"
+// let userPrompt = prompt(`Enter your full name.`)
+// alert(`${string.replace('the lazy dog', userPrompt)}`)
+//STEP 7
+// let string = "The quick brown fox jumps over the lazy dog"
+// let newWord = prompt('Enter a word')
+// alert(`${string.search(newWord)}`)
+//STEP 8
+// let old_string = "The quick brown fox jumps over the lazy dog"
+// let new_string = old_string.slice(31, 43)
+// alert(new_string.toUpperCase())
+//STEP 9
+// let string = "            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          "
+// alert(string.trim().toLowerCase())
+//STEP 10
+// let string = "the quick brown fox jumps over the lazy dog"
+// alert(string.charAt(0).toUpperCase() + string.slice(1))
